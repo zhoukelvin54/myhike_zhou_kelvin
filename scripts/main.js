2743675ed30e8abd57c8bdadbbdae39c542d3cce
Kelvin Zhou <szhou54@my.bcit.ca>
2022-03-01 13:38:51 -0800

    added demo09 modifications but need to add own content

---
+++ scripts/main.js
@@ -0,0 +1,120 @@
+function read_display_Quote() {
+    db.collection("quotes").doc("Tuesday")                                                      //name of the collection and documents should matach excatly with what you have in Firestore
+      .onSnapshot(TuesdayDoc => {                                                               //arrow notation
+           console.log("current document data: " + TuesdayDoc.data());                          //.data() returns data object
+           document.getElementById("quote-goes-here").innerHTML = TuesdayDoc.data().quote;      //using javascript to display the data on the right place
+           
+           //Here are other ways to access key:value data fields
+           //$('#quote-goes-here').text(c.data().quote);                                       //using jquery object dot notation
+           //$("#quote-goes-here").text(c.data()["quote"]);                                    //using json object indexing
+      })
+}
+read_display_Quote() 
+
+function insertName() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {                                                                 
+            // Do something for the current logged-in user here: 
+            console.log(user.uid);
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid);
+            //get the document for current user.
+            currentUser.get()
+                  .then(userDoc => {
+               var user_Name = userDoc.data().name;
+               console.log(user_Name);
+               //method #1:  insert with html only
+               //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
+               //method #2:  insert using jquery
+               $("#name-goes-here").text(user_Name);                         //using jquery
+            })
+        } else {
+            // No user is signed in.
+        }
+    });
+}
+insertName();
+
+function writeHikes() {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var hikesRef = db.collection("hikes");
+
+    hikesRef.add({
+        code:"BBY01",
+        name: "Burnaby Lake Park Trail",    //replace with your own city?
+        city: "Burnaby",
+        province: "BC",
+        level: "easy",
+        length: "10 km",
+        details: "Elmo goes here regularly"
+    });
+    hikesRef.add({
+        code:"AM01",
+        name: "Buntzen Lake Trail Trail",    //replace with your own city?
+        city: "Anmore",
+        province: "BC",
+        level: "moderate",
+        length: "10.5 km",
+        details: "Elmo goes here regularly"
+    });
+    hikesRef.add({
+        code:"NV01",
+        name: "Mount Seymoure Trail",    //replace with your own city?
+        city: "North Vancouver",
+        province: "BC",
+        level: "hard",
+        length: "8.2 km",
+        details: "Elmo goes here regularly"
+    });
+}
+
+//-----------------------------------------------
+// Create a "max" number of hike document objects
+//-----------------------------------------------
+function writeHikeData() {
+    max = 21;
+    //define a variable for the collection you want to create in Firestore to populate data
+    var hikesRef = db.collection("hikes");
+    for (i = 1; i <= max; i++) {
+        hikesRef.add({ //add to database, autogen ID
+            code: "id" + i,
+            name: "hike" + i,
+            details: "Elmo says this hike is amazing!  You will love going on hike" + i
+        })
+   }
+}
+
+// writeHikes();       //if you used method 1
+// writeHikeData();    //if you used method 2
+
+
+function displayCards(collection) {
+    let cardTemplate = document.getElementById("hikeCardTemplate");
+
+    db.collection(collection).get()
+        .then(snap => {
+            var i = 1;
+            snap.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name;   // get value of the "name" key
+                var details = doc.data().details;   // get value of the "details" key
+                let newcard = cardTemplate.content.cloneNode(true);
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-image').src = "./images/" + collection + ".jpg"; //hikes.jpg
+
+                //give unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+                i++;
+            })
+        })
+}
+
+displayCards("hikes");
